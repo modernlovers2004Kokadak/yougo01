777ae5542c75625d1eec69442d72aebdf2837f41
modernlovers2004Kokadak <modernlovers2004@gmail.com>
2026-08-06 07:38:02 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 const CACHE_PREFIX='riyoshi-glossary-';
-const CACHE=CACHE_PREFIX+'v3.0.193-20260805';
-const ASSETS=['./','./index.html','./infection-classification-guide.html','./infectionDiseaseData.js?v=3.0.193','./taisaku-youten.html','./dictionaryPage.js?v=3.0.193','./style.css?v=3.0.193','./manifest.webmanifest','./glossaryData.js?v=3.0.193','./glossaryQuizData.js?v=3.0.193','./cultureManagementData.js?v=3.0.193','./comprehensiveSupplementData.js?v=3.0.193','./glossaryApp.js?v=3.0.193','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./favicon-32.png'];
+const CACHE=CACHE_PREFIX+'v3.0.194-20260806';
+const ASSETS=['./','./index.html','./infection-classification-guide.html','./infectionDiseaseData.js?v=3.0.194','./taisaku-youten.html','./dictionaryPage.js?v=3.0.194','./style.css?v=3.0.194','./manifest.webmanifest','./glossaryData.js?v=3.0.194','./glossaryQuizData.js?v=3.0.194','./cultureManagementData.js?v=3.0.194','./comprehensiveSupplementData.js?v=3.0.194','./glossaryApp.js?v=3.0.194','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./favicon-32.png'];
 self.addEventListener('install',event=>{
   event.waitUntil((async()=>{
     const cache=await caches.open(CACHE);
